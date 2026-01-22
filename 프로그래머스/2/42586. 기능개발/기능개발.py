b0e826bdcc2ee8c08414f73c0692462e50e62d9c
Jiwon Hwang <83958344+Jiwon-0326@users.noreply.github.com>
2026-01-22 16:42:28 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,18 +1,18 @@
-import math 
+import math
 
 def solution(progresses, speeds):
     days = []
     for i, x in enumerate(progresses) :
         days.append(math.ceil((100-x)/speeds[i]))
-        
+    
     count = 1
     answer = []
-    for i in range(len(days)-1) : 
+    for i in range(len(progresses)-1) :
         if days[i+1] > days[i] :
             answer.append(count)
             count = 1
         else :
-            days[i+1] = days[i]
             count += 1
+            days[i+1] = days[i]
     answer.append(count)
     return answer
