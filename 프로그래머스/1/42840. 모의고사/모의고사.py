1726cf79cb1734e13398a0fc7ce765751dfd4100
Jiwon Hwang <83958344+Jiwon-0326@users.noreply.github.com>
2026-01-26 15:08:36 +0900

    [level 1] Title: 모의고사, Time: 3.95 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,22 +1,19 @@
 def solution(answers):
-    answer = [0 for i in range(3)]
-
-    man1 = [1,2,3,4,5]
-    man2 = [2,1,2,3,2,4,2,5]
-    man3 = [3,3,1,1,2,2,4,4,5,5]
+    one = [1,2,3,4,5]
+    two = [2, 1, 2, 3, 2, 4, 2, 5]
+    three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    score = [0,0,0]
+    answer = []
     
-    for i in range(len(answers)):
-        ans = answers[i]
-        if(man1[i%len(man1)] == ans):
-            answer[0] += 1
-        if(man2[i%len(man2)] == ans):
-            answer[1] += 1
-        if(man3[i%len(man3)] == ans):
-            answer[2] += 1     
+    for i in range(len(answers)) :
+        if (one[i%len(one)] == answers[i]) :
+            score[0] += 1
+        if (two[i%len(two)] == answers[i]) :
+            score[1] += 1
+        if (three[i%len(three)] == answers[i]) :
+            score[2] += 1
     
-    result = []
-    for i in range(len(answer)):
-        if(answer[i] == max(answer)):
-            result.append(i+1)
-    
-    return sorted(result)
+    for i in range(3) :
+        if score[i] == max(score) :
+            answer.append(i+1)
+    return answer
