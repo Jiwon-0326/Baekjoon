dffd30e02eb25cef7b8b6c3009ac0de9a2c9ec57
Jiwon Hwang <83958344+Jiwon-0326@users.noreply.github.com>
2026-01-28 18:08:17 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.93 ms, Memory: 25.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -2,10 +2,8 @@ def solution(participant, completion):
     participant.sort()
     completion.sort()
     
-    for i, j in zip(participant, completion):
-        if i!=j:
-            return i
-    return participant[-1]
-        
+    for i in range(len(completion)) :
+        if participant[i] != completion[i] :
+            return participant[i]
     
-        
+    return participant[-1]
