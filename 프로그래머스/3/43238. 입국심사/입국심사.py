a75f37d3d9d3f464b12f29c98752049aab17f6c2
Jiwon Hwang <83958344+Jiwon-0326@users.noreply.github.com>
2026-01-31 17:50:40 +0900

    [level 3] Title: 입국심사, Time: 569.61 ms, Memory: 13.3 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,18 +1,19 @@
 def solution(n, times):
     answer = 0
+    
     left = 1
-    right = n * max(times)
+    right = n * max(times)      # 제일 오래 걸리는 심사관이 모든 사람을 처리할 경우
     
     while left <= right :
         sum = 0
         mid = (left + right) // 2
-        
+    
         for i in times : 
-            sum += mid // i
-        if sum >= n :
+            sum += mid // i     # mid 시간동안 각 심사관이 처리할 수 있는 사람의 수를 합하기
+        if sum >= n :       # mid 시간동안 처리할 수 있는 총 사람이 n명보다 많으면 시간을 줄이기
             answer = mid
             right = mid - 1
-        else : 
+        else :              # mid 시간동안 처리할 수 있는 총 사람이 n명보다 적으면 시간을 늘리기
             left = mid + 1
     
     return answer
