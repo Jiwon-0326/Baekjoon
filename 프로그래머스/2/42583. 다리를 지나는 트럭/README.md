203cc537304d89bfc65fc0951659ae9e04dff5e1
Jiwon Hwang <83958344+Jiwon-0326@users.noreply.github.com>
2026-03-30 17:26:27 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 57.03 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583?language=python3#) 
 
 ### 성능 요약
 
-메모리: 9.24 MB, 시간: 56.01 ms
+메모리: 9.2 MB, 시간: 57.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 30일 16:36:26
+2026년 03월 30일 17:26:25
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,22 +1,26 @@
 from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
-    # (트럭의 최대 대수, 최대 무게, 트럭별 무게)
-    queue = deque([0]*bridge_length)       # 다리
-    time = 0                # 시간
-    bridge_weigh = 0        # 현재 다리 위 트럭 무게 합
+    time = 0
+    queue = deque([0]*bridge_length)
+    bridge_weights = 0
     truck_weights = deque(truck_weights)
     
-    while truck_weights or bridge_weigh > 0 :
+    while truck_weights or bridge_weights > 0 :
         out = queue.popleft()
-        bridge_weigh -= out
-        if truck_weights and bridge_weigh + truck_weights[0] <= weight :   # 다리에 트럭이 들어갈 수 있을 때
-            truck_now = truck_weights.popleft()
-            queue.append(truck_now)
-            bridge_weigh += truck_now
-            time += 1
-        else :              # 다리에 트럭이 못 들어갈 때
+        bridge_weights -= out
+        time += 1
+        if truck_weights and bridge_weights + truck_weights[0] <= weight :
+            now = truck_weights.popleft()
+            queue.append(now)
+            bridge_weights += now
+        else :
             queue.append(0)
-            time += 1  
     
     return time
+            
+            
+
+# 마지막 트럭 빠져나가는거 
+    
+    
